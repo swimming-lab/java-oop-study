720797eb2053abf1efc018cd220b1945cbf57d86
swimming <sooyoung209@naver.com>
2022-07-14 23:01:24 +0900

    Object 교재 학습 프로젝트

---
+++ sales-ticket/src/main/java/Bag.java
@@ -0,0 +1,46 @@
+public class Bag {
+
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(Long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, Long amount) {
+        this.amount = amount;
+        this.invitation = invitation;
+    }
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            minusAmount(ticket.getFee());
+            setTicket(ticket);
+            return ticket.getFee();
+        }
+    }
+
+    private boolean hasInvitation() {
+        return this.invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return this.ticket != null;
+    }
+
+    private void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    private void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
